556424bc70ca9ece0aef241ed10d6cd2c9e63fa0
Toni Panacek <toni.panacek@gmail.com>
2021-04-10 18:26:03 +0200

    pet app with bootstrap & simpleform

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/pets', to: 'pets#index'
+
+  get '/pets/new', to: 'pets#new', as: :new_pet
+  post '/pets', to: 'pets#create'
+
+
+  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch '/pets/:id', to: 'pets#update'
+
+  get '/pets/:id', to: 'pets#show', as: :pet
+  delete 'pets/:id', to: 'pets#destroy'
+
+  # resources :pets
 end
